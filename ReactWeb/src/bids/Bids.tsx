68e3fd5239bf014f3bad7fccba11feb0f266f1b9
mmtolzdorf <mtolzd70306@gmail.com>
2025-07-07 18:58:20 -0500

    created add bid ui elements

--- ReactWeb/src/bids/Bids.tsx
+++ ReactWeb/src/bids/Bids.tsx
@@ -19,10 +19,16 @@ const Bids = ({ item }: Props) => {
     amount: 0,
   };
 
-  //const { bid, setBid } = useState<Bid>(defaultBid);
+  const [bid, setBid] = useState<Bid>(defaultBid);
+
+  const onBidSubmit = () => {
+    addBidMutation.mutate(bid);
+    setBid(defaultBid);
+  };
 
   if (isPending) return <div>Loading...</div>;
   if (!isSuccess) return <div>Error loading bids</div>;
+
   return (
     <>
       <div className="row mt-4">
@@ -44,6 +50,35 @@ const Bids = ({ item }: Props) => {
                 ))}
             </tbody>
           </table>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <input
+            id="bidderName"
+            className="h-100"
+            type="text"
+            value={bid.bidderName}
+            onChange={(e) => setBid({ ...bid, bidderName: e.target.value })}
+            placeholder="Name"
+          ></input>
+        </div>
+        <div className="col-4">
+          <input
+            id="amount"
+            className="h-100"
+            type="number"
+            value={bid.amount}
+            onChange={(e) =>
+              setBid({ ...bid, amount: parseInt(e.target.value) })
+            }
+            placeholder="Amount"
+          ></input>
+        </div>
+        <div className="col-2">
+          <button className="btn btn-primary" onClick={() => onBidSubmit()}>
+            Submit
+          </button>
         </div>
       </div>
     </>
